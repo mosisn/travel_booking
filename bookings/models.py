0cd96295bd380eabbfd592ec0ff29476a343fab5
Mostafa <mostafa.saneisn@gmail.com>
2024-07-29 18:28:55 +0330

    Create Trip model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Trip(models.Model):
+    destination = models.CharField (max_length=100)
+    departure_date = models.DateField()
+    return_date = models.DateField()
+    number_of_travelers = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
