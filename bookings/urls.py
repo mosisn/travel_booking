151c0f724c68b7f2057d9840fd6c44a35335647f
Mostafa <mostafa.saneisn@gmail.com>
2024-07-31 12:05:35 +0330

    Add trip urls

---
+++ bookings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import delete_trip, update_trip, create_trip, list_trip
+
+
+urlpatterns = [
+    path('', list_trip, name='list_trip'),
+    path('new/', create_trip, name='create_trip'),
+    path('<int:pk>/edit/', update_trip, name='update_trip'),
+    path('<int:pk>/delete/', delete_trip, name='delete_trip'),
+]
