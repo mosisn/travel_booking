f339a464f3d9c8cb9a9aba60ce292059e8417758
Mostafa <mostafa.saneisn@gmail.com>
2024-08-02 02:42:42 +0330

    fix edit and delete urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,6 +5,6 @@ from .views import delete_trip, update_trip, create_trip, list_trip
 urlpatterns = [
     path('', list_trip, name='list_trip'),
     path('new/', create_trip, name='create_trip'),
-    path('<int:pk>/edit/', update_trip, name='update_trip'),
-    path('<int:pk>/delete/', delete_trip, name='delete_trip'),
+    path('<int:trip_id>/edit/', update_trip, name='update_trip'),
+    path('<int:trip_id>/delete/', delete_trip, name='delete_trip'),
 ]
