459b7bc5e94683a3a357a88be87df72814ee7ce2
Mostafa <mostafa.saneisn@gmail.com>
2024-08-02 02:36:51 +0330

    Create templates

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'travel_booking.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
